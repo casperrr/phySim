096c08cf7c8eac7fc7223956f7a4bcd0b3fbfb09
casperrr <casper.sebastien@gmail.com>
2022-11-26 22:30:16 +0000

    Bars Draw without color

--- simulations/bbleSort.js
+++ simulations/bbleSort.js
@@ -2,6 +2,8 @@ const canvas = document.querySelector('canvas');
 const c = canvas.getContext("2d");
 
 var barNum = 10;
+var dist = canvas.width/barNum;
+var hdist = canvas.height/barNum;
 var bars = [];
 
 class Bar{
@@ -17,4 +19,32 @@ function init(){
     }
 }
 
+function bg(){
+    c.fillStyle = '#181818';
+    c.fillRect(0,0,canvas.width,canvas.height);
+}
+
+function loop(){
+    bg();
+    drawBars();
+
+    requestAnimationFrame(loop);
+}
+
+function drawBars(){
+    for (i in bars){
+        var height = bars[i].height;
+        drawBar(i*dist,canvas.height+5,50,height*-hdist,"w")
+    }
+}
+
+function drawBar(x,y,width,height,color){
+    c.fillStyle = "#ffffff"; //this will be changed to color
+    c.beginPath();
+    c.roundRect(x,y,width,height,5);
+    c.fill();
+}
+
+
 init();
+loop();
