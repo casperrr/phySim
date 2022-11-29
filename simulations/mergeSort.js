d9a3d247119290bcc158a3a4e3472ec86471a7d6
casper <casper.sebastien@gmail.com>
2022-11-29 13:06:49 +0000

    Merge Sort Function Working

--- simulations/mergeSort.js
+++ simulations/mergeSort.js
@@ -142,7 +142,29 @@ async function bubbleSort(arr){
 }
 
 function mergeSort(arr){
+    let len = arr.length;
+    if (len <= 1) return arr;
 
+    let mid = Math.floor(len / 2);
+
+    let left = mergeSort(arr.slice(0,mid));
+    let right = mergeSort(arr.slice(mid));
+
+    return merge(left,right);
+}
+
+function merge(left,right){
+    let sortedArr = [];
+
+    while (left.length && right.length){
+        if (left[0].height < right[0].height){
+            sortedArr.push(left.shift());
+        }else {
+            sortedArr.push(right.shift());
+        }
+    }
+
+    return [...sortedArr, ...left,...right];
 }
 
 
